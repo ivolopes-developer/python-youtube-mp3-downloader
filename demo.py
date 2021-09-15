2a3de417e6ceafe07de0e1029d81982d1731a29e
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2021-09-15 14:31:53 +0100

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,26 @@
+import youtube_dl
+import os
+
+
+def run():
+    video_url = input("Youtube URL: ")
+    video_info = youtube_dl.YoutubeDL().extract_info(
+        url=video_url, download=False
+    )
+    filename = f"{video_info['title']}.mp3"
+
+    options = {
+        'format': 'bestaudio/best',
+        'keepvideo': False,
+        'outtmpl': filename,
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+
+    print("Download complete... {}".format(filename))
+
+
+if __name__ == '__main__':
+    os.system("cls")
+    run()
